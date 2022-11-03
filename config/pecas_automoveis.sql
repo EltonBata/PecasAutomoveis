400e344ff56d2a8f47e89c3282b426d1201c7722
EltonBata <eltonbata14@gmail.com>
2022-11-03 13:33:30 +0200

    actualizacao da bd

--- config/pecas_automoveis.sql
+++ config/pecas_automoveis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 01-Nov-2022 às 12:05
+-- Tempo de geração: 03-Nov-2022 às 11:32
 -- Versão do servidor: 8.0.27
 -- versão do PHP: 7.4.26
 
@@ -30,20 +30,19 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `administrador`;
 CREATE TABLE IF NOT EXISTS `administrador` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `apelido` varchar(100) NOT NULL,
+  `nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `apelido` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `data_nascimento` date NOT NULL,
-  `nacionalidade` int NOT NULL,
-  `nr_bi` varchar(100) NOT NULL,
-  `sexo` char(5) NOT NULL,
-  `morada` text NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contactos` varchar(100) NOT NULL,
+  `nacionalidade` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `nr_bi` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `sexo` char(5) COLLATE utf8mb4_general_ci NOT NULL,
+  `morada` text COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `contactos` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 
 --
 -- Estrutura da tabela `cliente`
@@ -52,12 +51,12 @@ CREATE TABLE IF NOT EXISTS `administrador` (
 DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE IF NOT EXISTS `cliente` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `apelido` varchar(100) NOT NULL,
-  `email` varchar(512) NOT NULL,
-  `contactos` varchar(100) NOT NULL,
-  `morada` text NOT NULL,
-  `nr_bi` varchar(50) NOT NULL,
+  `nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `apelido` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(512) COLLATE utf8mb4_general_ci NOT NULL,
+  `contactos` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `morada` text COLLATE utf8mb4_general_ci NOT NULL,
+  `nr_bi` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -72,10 +71,10 @@ CREATE TABLE IF NOT EXISTS `compra` (
   `id` int NOT NULL AUTO_INCREMENT,
   `data_compra` date NOT NULL,
   `data_entrega` date NOT NULL,
-  `local_entrega` text NOT NULL,
+  `local_entrega` text COLLATE utf8mb4_general_ci NOT NULL,
   `quantidade` int NOT NULL,
   `desconto` float NOT NULL,
-  `status` varchar(100) NOT NULL,
+  `status` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `total_pago` float NOT NULL,
   `id_cliente` int NOT NULL,
   `id_peca` int NOT NULL,
@@ -95,10 +94,10 @@ CREATE TABLE IF NOT EXISTS `compra` (
 DROP TABLE IF EXISTS `feedback`;
 CREATE TABLE IF NOT EXISTS `feedback` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mensagem` text NOT NULL,
+  `mensagem` text COLLATE utf8mb4_general_ci NOT NULL,
   `data` date NOT NULL,
-  `id_cliente` int NOT NULL,
-  `id_resposta` int NOT NULL,
+  `id_cliente` int DEFAULT NULL,
+  `id_resposta` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_cliente` (`id_cliente`),
   KEY `id_resposta` (`id_resposta`)
@@ -113,10 +112,10 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 DROP TABLE IF EXISTS `gestao`;
 CREATE TABLE IF NOT EXISTS `gestao` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `operacao` varchar(100) NOT NULL,
+  `operacao` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `data` date NOT NULL,
-  `id_gestor` int NOT NULL,
-  `id_peca` int NOT NULL,
+  `id_gestor` int DEFAULT NULL,
+  `id_peca` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_gestor` (`id_gestor`),
   KEY `id_peca` (`id_peca`)
@@ -131,24 +130,17 @@ CREATE TABLE IF NOT EXISTS `gestao` (
 DROP TABLE IF EXISTS `gestor`;
 CREATE TABLE IF NOT EXISTS `gestor` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `apelido` varchar(100) NOT NULL,
+  `nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `apelido` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `data_nascimento` date NOT NULL,
-  `nacionalidade` varchar(100) NOT NULL,
-  `nr_bi` varchar(100) NOT NULL,
-  `sexo` char(5) NOT NULL,
-  `morada` text NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contactos` varchar(100) NOT NULL,
+  `nacionalidade` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `nr_bi` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `sexo` char(5) COLLATE utf8mb4_general_ci NOT NULL,
+  `morada` text COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `contactos` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Extraindo dados da tabela `gestor`
---
-
-INSERT INTO `gestor` (`id`, `nome`, `apelido`, `data_nascimento`, `nacionalidade`, `nr_bi`, `sexo`, `morada`, `email`, `contactos`) VALUES
-(8, 'Jose Alberto', 'Kagame', '2022-11-02', 'Moçambicano', '123456789N', 'M', 'Maputo\r\nMatola', 'jAlbert@gmail.com', '843637263 ou 876252415');
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -159,15 +151,15 @@ INSERT INTO `gestor` (`id`, `nome`, `apelido`, `data_nascimento`, `nacionalidade
 DROP TABLE IF EXISTS `peca`;
 CREATE TABLE IF NOT EXISTS `peca` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(512) NOT NULL,
-  `tipo` varchar(100) NOT NULL,
-  `tamanho` varchar(50) NOT NULL,
-  `marca` varchar(512) NOT NULL,
+  `nome` varchar(512) COLLATE utf8mb4_general_ci NOT NULL,
+  `tipo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `tamanho` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `marca` varchar(512) COLLATE utf8mb4_general_ci NOT NULL,
   `preco` float NOT NULL,
   `data_fabrico` date NOT NULL,
-  `local_fabrico` varchar(512) NOT NULL,
-  `cor` varchar(50) NOT NULL,
-  `status` varchar(100) NOT NULL,
+  `local_fabrico` varchar(512) COLLATE utf8mb4_general_ci NOT NULL,
+  `cor` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -179,14 +171,16 @@ CREATE TABLE IF NOT EXISTS `peca` (
 
 DROP TABLE IF EXISTS `perfil`;
 CREATE TABLE IF NOT EXISTS `perfil` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(200) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `perfil` varchar(100) NOT NULL,
-  `id_gestor` int NOT NULL,
-  `id_administrador` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_perfil` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `perfil` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_gestor` int DEFAULT NULL,
+  `id_administrador` int DEFAULT NULL,
+  PRIMARY KEY (`id_perfil`),
+  KEY `id_gestor` (`id_gestor`),
+  KEY `id_administrador` (`id_administrador`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -197,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `perfil` (
 DROP TABLE IF EXISTS `resposta`;
 CREATE TABLE IF NOT EXISTS `resposta` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mensagem` text NOT NULL,
+  `mensagem` text COLLATE utf8mb4_general_ci NOT NULL,
   `data` date NOT NULL,
   `id_gestor` int NOT NULL,
   PRIMARY KEY (`id`),
