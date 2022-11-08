eeacb50704b11e0203c0ebfd1e092fc923694f91
Your Name <you@example.com>
2022-11-08 14:26:25 +0200

    clinte

--- gestao/views/clientes.php
+++ gestao/views/clientes.php
@@ -1,8 +1,11 @@
 <?php include_once './head.php'; ?>
 
+<?php include_once '../controllers/verClienteControler.php'; ?>
+
+
 <div class="container conteudo col-sm-10 p-3">
 
-    <h3 class="text-center"><i class="fa-solid fa-users-gear"></i> Clientes</h3>
+    <h3 class="text-center"><i class="fa-solid fa-users-gear"></i> (<?php echo $totalClientes; ?>)</h3>
 
     <div class="container mt-2">
         <table class="table">
@@ -15,7 +18,21 @@
                 <th>Contactos</th>
                 <th>Morada</th>
             </thead>
-        </table>
+     
+        <tbody>
+                <?php foreach ($cliente as $key => $value) { ?>
+                    <tr>
+                        <td><?php echo $value->id; ?></td>
+                        <td><?php echo $value->nome; ?></td>
+                        <td><?php echo $value->apelido; ?></td>
+                        <td><?php echo $value->email; ?></td>
+                        <td><?php echo $value->contactos; ?></td>
+                        <td><?php echo $value->morada; ?></td>
+                        <td><?php echo $value->nr_bi; ?></td>
+                        <?php }?>
+
+            </tbody>
+            </table> 
 
     </div>
     <div class="relatorio">
